da33b101edb61966dfc3656c2461e44995abe0b6
Sunny Hospital <119896595+shospital@users.noreply.github.com>
2024-07-25 16:17:23 -0700

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,16 @@
+// custom.js
+document.addEventListener("DOMContentLoaded", function() {
+  // List of URLs with target=_blank
+  const urlsToOpenInNewTab = [
+    "https://github.com/polarwatch/alaska-seaice",
+    "https://shinyfin.psmfc.org/ak-sst-mhw/",
+    "https://polarwatch.noaa.gov"
+  ];
+
+  urlsToOpenInNewTab.forEach(url => {
+    const link = document.querySelector(`a[href="${url}"]`);
+    if (link) {
+      link.setAttribute('target', '_blank');
+    }
+  });
+});
